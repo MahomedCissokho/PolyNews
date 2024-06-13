1c0c7ac722fb2808887998a0435867710206903b
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-06-13 11:13:31 +0000

    remove js page

--- index.php
+++ index.php
@@ -56,88 +56,88 @@ while ($category = $categoriesArray->fetch_row()) {
 
 <script>
 
-    const displayCategories = () => {
+const displayCategories = () => {
 
-        let dataCategories = <?php echo json_encode($dataCategories); ?>;
-        dataCategories.unshift({ 'id': 0, "libelle": "Accueil" });
-        console.log(dataCategories);
+let dataCategories = <?php echo json_encode($dataCategories); ?>;
+dataCategories.unshift({ 'id': 0, "libelle": "Accueil" });
+console.log(dataCategories);
 
-        let categoryDocument = document.getElementById("header");
-        for (let index = 0; index < dataCategories.length; index++) {
-            const element = dataCategories[index];
-            console.log(element.id);
-            const item = document.createElement("li")
-            item.setAttribute("class", "item")
-            const listContent = document.getElementById("list-item");
-            item.textContent = element.libelle
-            listContent.appendChild(item)
-            categoryDocument.appendChild(listContent)
-        }
+let categoryDocument = document.getElementById("header");
+for (let index = 0; index < dataCategories.length; index++) {
+    const element = dataCategories[index];
+    console.log(element.id);
+    const item = document.createElement("li")
+    item.setAttribute("class", "item")
+    const listContent = document.getElementById("list-item");
+    item.textContent = element.libelle
+    listContent.appendChild(item)
+    categoryDocument.appendChild(listContent)
+}
 
-    }
+}
 
-    const displayArticlesPerCategory = () => {
-        let articles = <?php echo json_encode($dataArticles); ?>;
-        console.log(articles);
-        let category = document.querySelectorAll(".item")
-        let content = document.getElementById("content")
+const displayArticlesPerCategory = () => {
+let articles = <?php echo json_encode($dataArticles); ?>;
+console.log(articles);
+let category = document.querySelectorAll(".item")
+let content = document.getElementById("content")
 
-        for (let k = 0; k < articles.length; k++) {
-            let div = document.createElement("div")
-            let h = document.createElement("h1");
-            h.innerHTML = articles[k].titre;
-            let p = document.createElement("p")
-            p.innerHTML = articles[k].contenu;
-            div.appendChild(h);
-            div.appendChild(p);
-            content.appendChild(div)
-        }
+for (let k = 0; k < articles.length; k++) {
+    let div = document.createElement("div")
+    let h = document.createElement("h1");
+    h.innerHTML = articles[k].titre;
+    let p = document.createElement("p")
+    p.innerHTML = articles[k].contenu;
+    div.appendChild(h);
+    div.appendChild(p);
+    content.appendChild(div)
+}
 
-        
-        for (let i = 0; i < category.length; i++) {
-            category[i].addEventListener("click", () => {
-                if (i == 0) {
-                    content.innerHTML = '';
-                    for (let j = 0; j < articles.length; j++) {
-                        let div, h, p;
-                        div = document.createElement("div")
-                        h = document.createElement("h1");
-                        p = document.createElement("p")
-                        h.innerHTML = articles[j].titre;
-                        p.innerHTML = articles[j].contenu;
-                        div.appendChild(h);
-                        div.appendChild(p);
-                        content.appendChild(div);
-                    }
-                } else {
-                    content.innerHTML = ''
-                    for (let index = 0; index < articles.length; index++) {
-                        const element = articles[index];
-                        if(element.categorie == i){
-                            let div1 = document.createElement("div");
-                            let p1 = document.createElement("p");
-                            let h1 = document.createElement("h1");
-                            h1.innerHTML = element.titre 
-                            p1.innerHTML = element.contenu
-                            div1.appendChild(h1);
-                            div1.appendChild(p1);
-                            content.appendChild(div1);
-                        }
-                        
-                    }
-                    if(content.textContent == '')
-                        content.innerHTML = `
-                            <h1>Aucun article trouve</h1>
-                        `
-                    
+
+for (let i = 0; i < category.length; i++) {
+    category[i].addEventListener("click", () => {
+        if (i == 0) {
+            content.innerHTML = '';
+            for (let j = 0; j < articles.length; j++) {
+                let div, h, p;
+                div = document.createElement("div")
+                h = document.createElement("h1");
+                p = document.createElement("p")
+                h.innerHTML = articles[j].titre;
+                p.innerHTML = articles[j].contenu;
+                div.appendChild(h);
+                div.appendChild(p);
+                content.appendChild(div);
+            }
+        } else {
+            content.innerHTML = ''
+            for (let index = 0; index < articles.length; index++) {
+                const element = articles[index];
+                if(element.categorie == i){
+                    let div1 = document.createElement("div");
+                    let p1 = document.createElement("p");
+                    let h1 = document.createElement("h1");
+                    h1.innerHTML = element.titre 
+                    p1.innerHTML = element.contenu
+                    div1.appendChild(h1);
+                    div1.appendChild(p1);
+                    content.appendChild(div1);
                 }
-            })
+                
+            }
+            if(content.textContent == '')
+                content.innerHTML = `
+                    <h1>Aucun article trouve</h1>
+                `
+            
         }
+    })
+}
 
-    }
+}
 
-    displayCategories()
-    displayArticlesPerCategory()
+displayCategories()
+displayArticlesPerCategory()
 
 </script>
 
